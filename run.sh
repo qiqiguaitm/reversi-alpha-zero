b4947b7fa873fc1084d8cd15d8ac12be1c5ed121
zhangtianming <zhangtianming@didichuxing.com>
2018-01-27 09:20:59 +0800

    u

--- run.sh
+++ run.sh
@@ -15,5 +15,5 @@ CUDA_VISIBLE_DEVICES='2' nohup ../anaconda3/bin/python src/reversi_zero/run.py s
 CUDA_VISIBLE_DEVICES='3' nohup ../anaconda3/bin/python src/reversi_zero/run.py self > out.self.`hostname`.11 2>&1 &
 
 
-#CUDA_VISIBLE_DEVICES='2' nohup ../anaconda3/bin/python -u  src/reversi_zero/run.py opt > out.opt.`hostname` 2>&1 &
+#nohup ../anaconda3/bin/python -u  src/reversi_zero/run.py opt > out.opt.`hostname` 2>&1 &
 #CUDA_VISIBLE_DEVICES='3' nohup ../anaconda3/bin/python -u src/reversi_zero/run.py eval > out.eval.`hostname` 2>&1 &
