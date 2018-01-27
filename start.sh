d89c1ae270caa52d9742ed6ce3d72e604b1deebc
qiqiguaitm <qiqiguaitm@sina.com>
2018-01-28 00:35:44 +0800

    u

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-rm out.*
-rm data/* -fr
-rm logs/* -fr
+#rm out.*
+#rm data/* -fr
+#rm logs/* -fr
 ps aux | grep -i python | awk '{system("kill "$2)}'
 ps aux | grep -i edax | awk '{system("kill "$2)}'
 #sh run.sh
